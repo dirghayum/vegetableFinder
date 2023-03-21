495d56f96f8b65445a2ec02da478a197ab832b9f
dirghayum <dirghayumainali@gmail.com>
2023-03-21 11:07:47 -0600

    Assignment II: modified

--- src/main/java/com/dmainali/vegitablefinder/controller/ProductController.java
+++ src/main/java/com/dmainali/vegitablefinder/controller/ProductController.java
@@ -1,26 +1,45 @@
 package com.dmainali.vegitablefinder.controller;
 
+import com.dmainali.vegitablefinder.entity.ResultWrapper;
+import com.dmainali.vegitablefinder.exception.VegetableException;
 import com.dmainali.vegitablefinder.service.ProductService;
 import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Set;
+import java.util.List;
+import java.util.Random;
 
 
 @RestController
 public class ProductController {
 
     private ProductService productService;
+    ResultWrapper wrapper = new ResultWrapper();
 
     @Autowired
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
 
+
     @GetMapping(value= "/getList")
-    public Set<Pair<String,String>> findAll() {
+    public List<Pair<String,String>> findAll() {
         return productService.readProducts();
+    }
+
+    @GetMapping(value= "/getVegetables")
+    public ResultWrapper findRandom() {
+        try {
+            List<Pair<String, String>> vlist = productService.readProducts();
+            Random rand = new Random();
+            int randomIndex = rand.nextInt(vlist.size());
+            vlist.get(randomIndex);
+            wrapper.setVegetable(vlist.get(randomIndex));
+        }catch(VegetableException exception){
+            wrapper.setVegetableException(exception);
+        }
+        return wrapper;
     }
 }
--- src/main/java/com/dmainali/vegitablefinder/service/ProductService.java
+++ src/main/java/com/dmainali/vegitablefinder/service/ProductService.java
@@ -4,13 +4,13 @@ import com.dmainali.vegitablefinder.dataprocessor.ProductReader;
 import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.stereotype.Service;
 
-import java.util.Set;
+import java.util.List;
 
 
 @Service
 public class ProductService {
 
-    public Set<Pair<String,String>> readProducts(){
+    public List<Pair<String,String>> readProducts(){
         ProductReader productReader = new ProductReader();
         return productReader.readProducts();
     }
